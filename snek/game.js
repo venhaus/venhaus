a40ab92fe2c118befdc607cd0ebb8fb12808739f
Paul Venhaus <paul.venhaus@gmail.com>
2017-08-13 16:32:47 +0200

    Finished main game

--- snek/game.js
+++ snek/game.js
@@ -1,9 +1,8 @@
-// TODO remove console logs
-// TODO only execute code if canvas is supported by browser
-// TODO prevent 180 turns
-// TODO game win conditions
-// FIXME apple spawns in Snek
-// TODO snake collision detection
+// TODO (2) only execute code if canvas is supported by browser
+// TODO (2) restart button
+// TODO (2) option for teleporting borders
+
+// TODO (3) remove console logs
 
 var currentDirection;
 var previousDirection;
@@ -17,29 +16,46 @@ var pixelHeight = gameHeight / verticalPixels;
 var pixelWidth = gameWidth / horizontalPixels;
 var applePos;
 var snakeArray = [];
+var gameLost = false;
+var delay = 300;
 
 function Point(x = 0, y = 0) {
   this.x = x;
   this.y = y;
 }
+Point.prototype.imitate = function(point) {
+  this.x = point.x;
+  this.y = point.y;
+  return this;
+};
+Point.prototype.equals = function(point) {
+  return (this.x == point.x && this.y == point.y);
+};
 
 function rndPoint() {
-  return (new Point(Math.floor(Math.random()*10), Math.floor(Math.random()*10)));
+  return (new Point(Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)));
+}
+
+function setApplePos() {
+  applePos = rndPoint();
+  //var appleFlag = false;
+  for (let index in snakeArray) {
+    if (snakeArray[index].equals(applePos)) {
+      setApplePos();
+      break;
+    }
+  }
 }
 
 function move(event) {
   if (event.keyCode == 37) {
     currentDirection = "left";
-    console.log(currentDirection);
   } else if (event.keyCode == 38) {
     currentDirection = "up";
-    console.log(currentDirection);
   } else if (event.keyCode == 39) {
     currentDirection = "right";
-    console.log(currentDirection);
   } else if (event.keyCode == 40) {
     currentDirection = "down";
-    console.log(currentDirection);
   }
 }
 
@@ -47,14 +63,39 @@ function paintContext() {
   context.fillStyle = "white";
   context.fillRect(0, 0, gameWidth, gameHeight);
   context.fillStyle = "red";
-  context.fillRect(applePos.x * pixelWidth, applePos.y * pixelHeight, pixelWidth, pixelHeight);
+  context.fillRect(Math.floor(applePos.x * pixelWidth + pixelWidth * 0.02), Math.floor(applePos.y * pixelHeight + pixelWidth * 0.02), Math.floor(pixelWidth * 0.96), Math.floor(pixelHeight * 0.96));
   context.fillStyle = "green";
   for (let index in snakeArray) {
-    context.fillRect(snakeArray[index].x * pixelWidth, snakeArray[index].y * pixelHeight, pixelWidth, pixelHeight);
+    context.fillRect(Math.floor(snakeArray[index].x * pixelWidth + pixelWidth * 0.02), Math.floor(snakeArray[index].y * pixelHeight + pixelWidth * 0.02), Math.floor(pixelWidth * 0.96), Math.floor(pixelHeight * 0.96));
+  }
+  //Paint Snek eyes
+  context.fillStyle = "black";
+  if (currentDirection == "down") {
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
+  } else if (currentDirection == "up") {
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
+  } else if (currentDirection == "left") {
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
+  } else if (currentDirection == "right") {
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
+    context.fillRect(Math.floor(snakeArray[0].x*pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y*pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight *0.1));
   }
 }
 
 function game() {
+  //Move Snek tail
+  for (let i = snakeArray.length - 1; i > 0; i--) {
+    snakeArray[i] = new Point(snakeArray[i - 1].x, snakeArray[i - 1].y);
+  }
+
+  //Prevent Snek from turning 180 degrees
+  if (currentDirection == "down" && previousDirection == "up" || currentDirection == "up" && previousDirection == "down" || currentDirection == "left" && previousDirection == "right" || currentDirection == "right" && previousDirection == "left") {
+    currentDirection = previousDirection;
+  }
+
   //Move Snek head
   if (currentDirection == "left" && snakeArray[0].x != 0) {
     snakeArray[0].x--;
@@ -65,23 +106,32 @@ function game() {
   } else if (currentDirection == "down" && snakeArray[0].y != verticalPixels - 1) {
     snakeArray[0].y++;
   }
+  previousDirection = currentDirection;
 
-  //Move Snek tail
-  for (let i = snakeArray.length - 1; i > 0; i--) {
-    snakeArray[i] = new Point(snakeArray[i-1].x, snakeArray[i-1].y);
+  //Check if Snek touches itself
+  if (snakeArray.length > 1) {
+    let snakeSubArray = snakeArray.slice(1);
+    for (let index in snakeSubArray) {
+      if (snakeArray[0].equals(snakeSubArray[index])) {
+        gameLost = true;
+        alert("You lost! \n Final Score: " + snakeArray.length);
+      }
+    }
   }
 
   //If Snek touches apple...
   if (snakeArray[0].x == applePos.x && snakeArray[0].y == applePos.y) {
-    snakeArray.push(snakeArray[snakeArray.length - 1]);
-    applePos = rndPoint();
-    console.log(snakeArray.length);
-
+    snakeArray.push(new Point().imitate(snakeArray[snakeArray.length - 1]));
+    setApplePos();
+    delay = delay * 0.95;
   }
 
-  //Paint everything
-  paintContext();
-}
+  //Update canvas context or end game
+  if (!gameLost) {
+    paintContext();
+    window.setTimeout(game, delay);
+  }
+} //end game()
 
 document.addEventListener("keydown", move);
 
@@ -89,7 +139,6 @@ gameCanvas.setAttribute("height", gameHeight + "px");
 gameCanvas.setAttribute("width", gameWidth + "px");
 
 //Initialize
-applePos = rndPoint();
 snakeArray[0] = rndPoint();
-
-window.setInterval(game, 300);
+setApplePos();
+window.setTimeout(game, delay);
