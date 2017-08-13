3e7daa1aab6928d120007aecad874907446ff36b
Paul Venhaus <paul.venhaus@gmail.com>
2017-08-13 23:19:13 +0200

    Solution?

--- snek/game.js
+++ snek/game.js
@@ -159,7 +159,7 @@ function game() {
     paintContext();
     window.setTimeout(game, delay);
   } else {
-    alert("You lost! :( \n Final Score: " + snakeArray.length);
+    alert("You lost! :) \n Final Score: " + snakeArray.length);
     initialize();
   }
 } //end game()
@@ -183,9 +183,6 @@ function swipeDetect(swipeArea, callback) {
     var touchobj = e.changedTouches[0];
     distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
     distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
-    if (Math.abs(distX) > 50) {
-      alert("blubb");
-    }
     if (Math.abs(distX) > 50 && Math.abs(distX) > Math.abs(distY)) { // 2nd condition for horizontal swipe met
       swipedir = (distX < 0) ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
     } else if (Math.abs(distY) > 50 && Math.abs(distY) > Math.abs(distX)) { // 2nd condition for vertical swipe met
