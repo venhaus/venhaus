3d21758476207122ea46634169f83615b05ff903
Paul Venhaus <paul.venhaus@gmail.com>
2017-08-14 19:37:03 +0200

    Code clean up and full page swipe detection

--- snek/game.js
+++ snek/game.js
@@ -1,28 +1,29 @@
-// TODO (2) only execute code if canvas is supported by browser
-// TODO (2) buttons for manual steering
-// TODO (2) mobile version
+// TODO (2) larger game area on mobile
 
-// TODO (3) set delay
 // TODO (3) remove console logs
 
 var currentDirection;
 var previousDirection;
 var gameCanvas = document.getElementById("gameCanvas");
-var context = gameCanvas.getContext("2d");
-var gameHeight = Math.min(window.innerHeight, window.innerWidth) * 0.8;
-var gameWidth = gameHeight;
+var context;
+var gameHeight;
+var gameWidth;
 var verticalPixels = 10;
 var horizontalPixels = 10;
-var pixelHeight = gameHeight / verticalPixels;
-var pixelWidth = gameWidth / horizontalPixels;
+var pixelHeight;
+var pixelWidth;
+var pixelHeightAdj;
+var pixelWidthAdj;
+var pixelHeightEyes;
+var pixelWidthEyes;
 var applePos;
 var snakeArray = [];
 var gameLost = false;
 var delay;
 var scoreDisplay = document.getElementById("scoreDisplay");
+var swipeArea = document.body;
 
 function initialize() {
-  console.log("Initialize");
   delay = 300;
   gameLost = false;
   scoreDisplay.innerHTML = "Score: 1";
@@ -31,6 +32,7 @@ function initialize() {
   snakeArray = [];
   snakeArray[0] = rndPoint();
   setApplePos();
+  setGameSize();
   paintContext();
   game();
 }
@@ -79,42 +81,81 @@ function paintContext() {
   context.fillStyle = "black";
   context.fillRect(0, 0, gameWidth, gameHeight);
   context.fillStyle = "red";
-  context.fillRect(Math.floor(applePos.x * pixelWidth + pixelWidth * 0.02), Math.floor(applePos.y * pixelHeight + pixelWidth * 0.02), Math.floor(pixelWidth * 0.96), Math.floor(pixelHeight * 0.96));
+  context.fillRect(Math.floor(applePos.x * pixelWidth + pixelWidth * 0.02), Math.floor(applePos.y * pixelHeight + pixelWidth * 0.02), pixelWidthAdj, pixelHeightAdj);
   context.fillStyle = "lime";
   for (let index in snakeArray) {
-    context.fillRect(Math.floor(snakeArray[index].x * pixelWidth + pixelWidth * 0.02), Math.floor(snakeArray[index].y * pixelHeight + pixelWidth * 0.02), Math.floor(pixelWidth * 0.96), Math.floor(pixelHeight * 0.96));
+    context.fillRect(Math.floor(snakeArray[index].x * pixelWidth + pixelWidth * 0.02), Math.floor(snakeArray[index].y * pixelHeight + pixelWidth * 0.02), pixelWidthAdj, pixelHeightAdj);
   }
   //Paint Snek eyes
   context.fillStyle = "black";
   if (currentDirection == "down") {
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), pixelWidthEyes, pixelHeightEyes);
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), pixelWidthEyes, pixelHeightEyes);
   } else if (currentDirection == "up") {
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), pixelWidthEyes, pixelHeightEyes);
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), pixelWidthEyes, pixelHeightEyes);
   } else if (currentDirection == "left") {
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), pixelWidthEyes, pixelHeightEyes);
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.2), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), pixelWidthEyes, pixelHeightEyes);
   } else if (currentDirection == "right") {
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
-    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), Math.floor(pixelWidth * 0.1), Math.floor(pixelHeight * 0.1));
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.2), pixelWidthEyes, pixelHeightEyes);
+    context.fillRect(Math.floor(snakeArray[0].x * pixelWidth + pixelWidth * 0.7), Math.floor(snakeArray[0].y * pixelHeight + pixelHeight * 0.7), pixelWidthEyes, pixelHeightEyes);
   }
 }
 
 function setGameSize() {
-  gameHeight = Math.min(window.innerHeight, window.innerWidth) * 0.7;
+  gameHeight = Math.min(window.innerHeight, window.innerWidth) * 0.75;
   gameWidth = gameHeight;
   pixelHeight = gameHeight / verticalPixels;
   pixelWidth = gameWidth / horizontalPixels;
+  pixelHeightAdj = Math.floor(pixelHeight * 0.96);
+  pixelWidthAdj = Math.floor(pixelWidth * 0.96);
+  pixelHeightEyes = Math.floor(pixelHeight * 0.1);
+  pixelWidthEyes = Math.floor(pixelWidth * 0.1);
   gameCanvas.setAttribute("height", gameHeight + "px");
   gameCanvas.setAttribute("width", gameWidth + "px");
   paintContext();
-
 }
+
+function swipeDetect(swipeArea, callback) {
+  var swipedir,
+    startX,
+    startY,
+    distX,
+    distY;
+
+  swipeArea.addEventListener('touchstart', function(e) {
+    var touchobj = e.changedTouches[0];
+    startX = touchobj.pageX;
+    startY = touchobj.pageY;
+    e.preventDefault();
+  });
+
+  swipeArea.addEventListener('touchmove', function(e) {
+    var touchobj = e.changedTouches[0];
+    distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+    distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+    if (Math.abs(distX) > 25 && Math.abs(distX) > Math.abs(distY)) { // 2nd condition for horizontal swipe met
+      swipedir = (distX < 0) ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
+      startX = touchobj.pageX;
+      startY = touchobj.pageY;
+    } else if (Math.abs(distY) > 25 && Math.abs(distY) > Math.abs(distX)) { // 2nd condition for vertical swipe met
+      swipedir = (distY < 0) ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
+      startX = touchobj.pageX;
+      startY = touchobj.pageY;
+    }
+    callback(swipedir);
+
+    e.preventDefault(); // prevent scrolling when inside swipeArea
+  });
+}
+
+swipeDetect(swipeArea, function(swipedir) {
+  currentDirection = swipedir;
+});
 
 function game() {
   //Move Snek tail
-  console.log("game");
   for (let i = snakeArray.length - 1; i > 0; i--) {
     snakeArray[i] = new Point(snakeArray[i - 1].x, snakeArray[i - 1].y);
   }
@@ -146,7 +187,7 @@ function game() {
     }
   }
 
-  //If Snek touches apple...
+  //If Snek touches apple
   if (snakeArray[0].x == applePos.x && snakeArray[0].y == applePos.y) {
     snakeArray.push(new Point().imitate(snakeArray[snakeArray.length - 1]));
     scoreDisplay.innerHTML = "Score: " + snakeArray.length;
@@ -164,47 +205,11 @@ function game() {
   }
 } //end game()
 
-
-function swipeDetect(swipeArea, callback) {
-  var swipedir,
-    startX,
-    startY,
-    distX,
-    distY;
-
-  swipeArea.addEventListener('touchstart', function(e) {
-    var touchobj = e.changedTouches[0];
-    startX = touchobj.pageX;
-    startY = touchobj.pageY;
-    e.preventDefault();
-  });
-
-  swipeArea.addEventListener('touchmove', function(e) {
-    var touchobj = e.changedTouches[0];
-    distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
-    distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
-    if (Math.abs(distX) > 50 && Math.abs(distX) > Math.abs(distY)) { // 2nd condition for horizontal swipe met
-      swipedir = (distX < 0) ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
-      startX = touchobj.pageX;
-      startY = touchobj.pageY;
-    } else if (Math.abs(distY) > 50 && Math.abs(distY) > Math.abs(distX)) { // 2nd condition for vertical swipe met
-      swipedir = (distY < 0) ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
-      startX = touchobj.pageX;
-      startY = touchobj.pageY;
-    }
-    callback(swipedir);
-
-    e.preventDefault(); // prevent scrolling when inside swipeArea
-  });
+if (gameCanvas.getContext("2d")){
+  document.addEventListener("keydown", move);
+  context = gameCanvas.getContext("2d");
+  gameCanvas.setAttribute("height", gameHeight + "px");
+  gameCanvas.setAttribute("width", gameWidth + "px");
+  window.addEventListener("resize", setGameSize);
+  initialize();
 }
-
-var swipeArea = document.getElementById('swipeZone');
-swipeDetect(swipeArea, function(swipedir) {
-  currentDirection = swipedir;
-});
-
-document.addEventListener("keydown", move);
-gameCanvas.setAttribute("height", gameHeight + "px");
-gameCanvas.setAttribute("width", gameWidth + "px");
-window.addEventListener("resize", setGameSize);
-initialize();
